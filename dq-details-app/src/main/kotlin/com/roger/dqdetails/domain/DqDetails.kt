3539f561ab0fc29d52879f25685092dee37a5248
team.hiro <thomas.rawlings>
2020-07-02 10:57:32 +0100

    Federated the schema and joined DqDeatils to Service

--- dq-details-app/src/main/kotlin/com/roger/dqdetails/domain/DqDetails.kt
+++ dq-details-app/src/main/kotlin/com/roger/dqdetails/domain/DqDetails.kt
@@ -1,19 +1,30 @@
 package com.roger.dqdetails.domain
 
+import com.expediagroup.graphql.federation.directives.ExtendsDirective
+import com.expediagroup.graphql.federation.directives.ExternalDirective
+import com.expediagroup.graphql.federation.directives.FieldSet
+import com.expediagroup.graphql.federation.directives.KeyDirective
+
+@KeyDirective(fields = FieldSet("serviceId"))
 data class DqDetails(
         val serviceId: Long,
         val type: String,
-        val firstNameOrInitial: String,
+        val firstNameOrInitial: String?,
         val surname: String,
-        val specialEquipment: String,
-        val specialTypeface: String,
-        val premises: String,
-        val thoroughfare: String,
-        val locality: String,
         val postcode: String,
         val recordId: String,
-        val marketingPreference: String,
-        val nddReferenceNumber: String,
-        val customerType: String) {
+        val customerType: String)
 
-}
+data class DqDetailsInput(
+        val serviceId: Long,
+        val type: String,
+        val firstNameOrInitial: String?,
+        val surname: String,
+        val postcode: String,
+        val recordId: String,
+        val customerType: String)
+
+@KeyDirective(fields = FieldSet("id"))
+@ExtendsDirective
+data class Service(@property:ExternalDirective val id: Long,
+                   val dqDetails: DqDetails)
--- dq-details-app/src/main/kotlin/com/roger/dqdetails/domain/DqDetailsQuery.kt
+++ dq-details-app/src/main/kotlin/com/roger/dqdetails/domain/DqDetailsQuery.kt
@@ -1,19 +1,52 @@
 package com.roger.dqdetails.domain
 
-import com.expediagroup.graphql.SchemaGeneratorConfig
-import com.expediagroup.graphql.TopLevelObject
+import com.expediagroup.graphql.federation.execution.FederatedTypeRegistry
+import com.expediagroup.graphql.federation.execution.FederatedTypeResolver
+import com.expediagroup.graphql.spring.operations.Mutation
 import com.expediagroup.graphql.spring.operations.Query
-import com.expediagroup.graphql.toSchema
+import graphql.schema.DataFetchingEnvironment
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+
+val dqDetailsMap = mutableMapOf<Long, DqDetails>()
 
 @Component
 class DqDetailsQuery : Query {
-    fun dqDetails(serviceId: Long): DqDetails {
-        return DqDetails(serviceId, "0001", "Bob", "surname", "specialEquipment", "specialTypeface", "premises", "thoroughfare", "locality", "postcode", "recordId", "marketingPreferences", "nddReferenceNumber", "customerType")
+    fun dqDetails(serviceId: Long) = dqDetailsMap[serviceId]
+}
+
+@Component
+class DqDetailsMutation : Mutation {
+    fun dqDetails(dqDetails: DqDetailsInput): DqDetails {
+        if (dqDetailsMap[dqDetails.serviceId] != null) {
+            dqDetailsMap[dqDetails.serviceId] = DqDetails(
+                    dqDetails.serviceId,
+                    dqDetails.type,
+                    dqDetails.firstNameOrInitial,
+                    dqDetails.surname,
+                    dqDetails.postcode,
+                    dqDetails.recordId,
+                    dqDetails.customerType)
+        }
+
+
+        dqDetailsMap[dqDetails.serviceId] = DqDetails(
+                dqDetails.serviceId,
+                dqDetails.type,
+                dqDetails.firstNameOrInitial,
+                dqDetails.surname,
+                dqDetails.postcode,
+                dqDetails.recordId,
+                dqDetails.customerType)
+        return dqDetailsMap[dqDetails.serviceId]!!
     }
 }
 
-// Generate the schema
-val config = SchemaGeneratorConfig(supportedPackages = listOf("com.roger.dqdetails.domain"))
-val queries = listOf(TopLevelObject(DqDetailsQuery()))
-val schema = toSchema(config, queries)
+private val dqDetailsResolver = object : FederatedTypeResolver<DqDetails> {
+    override suspend fun resolve(environment: DataFetchingEnvironment, representations: List<Map<String, Any>>): List<DqDetails?> = representations.map {
+        dqDetailsMap[it["serviceId"]]
+    }
+}
+
+@Bean
+fun federatedTypeRegistry() = FederatedTypeRegistry(mapOf("DqDetails" to dqDetailsResolver))
